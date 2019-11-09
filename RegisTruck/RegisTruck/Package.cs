d99ba36111c114f1becc3ecaa197b4d762df6e31
Szücs József <szujoz95@gmail.com>
2019-11-09 21:57:51 +0100

    Project is created.

---
+++ RegisTruck/RegisTruck/Package.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegisTruck
+{
+    enum PackageType
+    {
+        Invalid = 0,
+        Normal,
+        Fragile,
+        Custom,
+        Priority
+    }
+
+    class Package
+    {
+        public int Id { get; set; }
+
+        public int DestinationStationId { get; set; }
+
+        public int Weight { get; set; }
+
+        public String Deadline { get; set; }
+
+        public PackageType Type { get; set; }
+
+        public String Description { get; set; }
+
+
+        public Package()
+        {
+            Id = -1;
+            DestinationStationId = -1;
+            Weight = -1;
+            Deadline = "2000.01.01";
+            Type = PackageType.Invalid;
+            Description = "";
+        }
+    }
+}
---
+++ RegisTruck/RegisTruckTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RegisTruckTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegisTruckTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ccf87de-6689-400b-a5b4-a8ad58da8578")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
